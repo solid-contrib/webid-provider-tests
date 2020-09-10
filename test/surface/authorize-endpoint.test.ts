00f68204f494a11a7e21de9a0e17502691fb476a
Michiel de Jong <michiel@unhosted.org>
2020-09-10 12:48:15 +0200

    I think this should fix #22

--- test/surface/authorize-endpoint.test.ts
+++ test/surface/authorize-endpoint.test.ts
@@ -1,35 +1,25 @@
 import fetch from "node-fetch";
+import { getCookie } from "../helpers/getCookie";
 
 const SERVER_ROOT = process.env.SERVER_ROOT || "https://server";
-const LOGIN_URL = `${SERVER_ROOT}/login/password`;
-const query1 = "?response_type=id_token%20code&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fredirect&scope=openid%20profile%20offline_access&client_id=coolApp1&code_challenge_method=S256&code_challenge=M3CBok-0kQFc0GUz2YD90cFee0XzTTru3Eaj0Ubm-oc&state=84ae2b48-eb1b-4000-8782-ac1cd748aeb0";
-const query2 = "?response_type=id_token%20code&redirect_uri=http%3A%2F%2Flocalhost%3A3002%2Fredirect&scope=openid%20profile%20offline_access&client_id=coolApp2&code_challenge_method=S256&code_challenge=M3CBok-0kQFc0GUz2YD90cFee0XzTTru3Eaj0Ubm-oc&state=84ae2b48-eb1b-4000-8782-ac1cd748aeb0";
-
-async function getCookie() {
-  const result = await fetch(`${SERVER_ROOT}/login/password`, {
-    headers: {
-      "content-type": "application/x-www-form-urlencoded"
-    },
-    body: "username=alice&password=123",
-    method: "POST",
-    redirect: "manual"
-  });
-  return result.headers.get('set-cookie');
-}
+const LOGIN_URL = `${SERVER_ROOT}/login`;
+const query1 =
+  "?response_type=id_token%20code&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fredirect&scope=openid%20profile%20offline_access&client_id=coolApp1&code_challenge_method=S256&code_challenge=M3CBok-0kQFc0GUz2YD90cFee0XzTTru3Eaj0Ubm-oc&state=84ae2b48-eb1b-4000-8782-ac1cd748aeb0";
+// const query2 =
+//   "?response_type=id_token%20code&redirect_uri=http%3A%2F%2Flocalhost%3A3002%2Fredirect&scope=openid%20profile%20offline_access&client_id=coolApp2&code_challenge_method=S256&code_challenge=M3CBok-0kQFc0GUz2YD90cFee0XzTTru3Eaj0Ubm-oc&state=84ae2b48-eb1b-4000-8782-ac1cd748aeb0";
 
 describe("The server's authorize endpoint", () => {
-  let authorizationEndpoint
-  let cookie
+  let authorizationEndpoint;
+  let cookie;
 
   beforeAll(async () => {
+    cookie = await getCookie(LOGIN_URL);
     const configFetchResult = await fetch(
       `${SERVER_ROOT}/.well-known/openid-configuration`
     );
     const body = await configFetchResult.text();
     const configObj = JSON.parse(body);
     authorizationEndpoint = configObj.authorization_endpoint;
-    cookie = await getCookie();
-
   });
 
   test("the authorize endpoint is within the system under test", async () => {
@@ -38,39 +28,43 @@ describe("The server's authorize endpoint", () => {
 
   test("the authorize URL without cookie sends you to login", async () => {
     const fetchResult = await fetch(authorizationEndpoint + query1, {
-      redirect: "manual"
+      redirect: "manual",
     });
     expect(fetchResult.status).toEqual(302);
-    expect(fetchResult.headers.get('location')).toEqual(`${SERVER_ROOT}/login` + query1);
+    expect(fetchResult.headers.get("location")).toEqual(
+      `${SERVER_ROOT}/login` + query1
+    );
   });
 
   test("when redirected to login, you see a html form", async () => {
     const fetchResult = await fetch(authorizationEndpoint + query1, {
-      redirect: "follow"
+      redirect: "follow",
     });
     expect(fetchResult.status).toEqual(200);
     const body = await fetchResult.text();
     expect(body.indexOf("form")).not.toEqual(-1);
   });
 
-  test("the authorize URL with cookie sends you to consent", async () => {
+  test.only("the authorize URL with cookie sends you to consent", async () => {
     const fetchResult = await fetch(authorizationEndpoint + query1, {
       headers: {
-        cookie
+        cookie,
       },
-      redirect: "manual"
+      redirect: "manual",
     });
     expect(fetchResult.status).toEqual(302);
-    expect(fetchResult.headers.get('location')).toEqual(`${SERVER_ROOT}/sharing${query1}`);
+    expect(fetchResult.headers.get("location")).toEqual(
+      `${SERVER_ROOT}/sharing${query1}`
+    );
   });
 
   test("when redirected to consent, there is a html form", async () => {
     // This test uses the unconsented coolApp1
     const fetchResult = await fetch(authorizationEndpoint + query1, {
       headers: {
-        cookie
+        cookie,
       },
-      redirect: "follow"
+      redirect: "follow",
     });
     expect(fetchResult.status).toEqual(200);
     const body = await fetchResult.text();
@@ -81,40 +75,51 @@ describe("The server's authorize endpoint", () => {
     // This test uses the unconsented coolApp1
     const fetchResult = await fetch(authorizationEndpoint + query1, {
       headers: {
-        cookie
+        cookie,
       },
-      redirect: "manual"
+      redirect: "manual",
     });
     expect(fetchResult.status).toEqual(302);
-    expect(fetchResult.headers.get('location')).toEqual(`${SERVER_ROOT}/sharing?response_type=id_token%20code&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fredirect&scope=openid%20profile%20offline_access&client_id=coolApp1&code_challenge_method=S256&code_challenge=M3CBok-0kQFc0GUz2YD90cFee0XzTTru3Eaj0Ubm-oc&state=84ae2b48-eb1b-4000-8782-ac1cd748aeb0`);
+    expect(fetchResult.headers.get("location")).toEqual(
+      `${SERVER_ROOT}/sharing?response_type=id_token%20code&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fredirect&scope=openid%20profile%20offline_access&client_id=coolApp1&code_challenge_method=S256&code_challenge=M3CBok-0kQFc0GUz2YD90cFee0XzTTru3Eaj0Ubm-oc&state=84ae2b48-eb1b-4000-8782-ac1cd748aeb0`
+    );
   });
 
   test("Giving consent redirects you back to authorize", async () => {
     // This test uses the consented coolApp2
     const fetchResult = await fetch(`${SERVER_ROOT}/sharing`, {
-      "headers": {
+      headers: {
         "content-type": "application/x-www-form-urlencoded",
         "upgrade-insecure-requests": "1",
-        cookie
+        cookie,
       },
-      "body": "access_mode=Read&access_mode=Append&access_mode=Write&access_mode=Control&consent=true&response_type=id_token+code&display=&scope=openid+profile+offline_access&client_id=coolApp2&redirect_uri=http%3A%2F%2Flocalhost%3A3002%2Fredirect&state=84ae2b48-eb1b-4000-8782-ac1cd748aeb0&nonce=&request=",
-      "method": "POST",
-      redirect: "manual"
+      body:
+        "access_mode=Read&access_mode=Append&access_mode=Write&access_mode=Control&consent=true&response_type=id_token+code&display=&scope=openid+profile+offline_access&client_id=coolApp2&redirect_uri=http%3A%2F%2Flocalhost%3A3002%2Fredirect&state=84ae2b48-eb1b-4000-8782-ac1cd748aeb0&nonce=&request=",
+      method: "POST",
+      redirect: "manual",
     });
     expect(fetchResult.status).toEqual(302);
-    expect(fetchResult.headers.get('location')).toEqual(`${authorizationEndpoint}?response_type=id_token%20code&display=&scope=openid%20profile%20offline_access&client_id=coolApp2&redirect_uri=http%3A%2F%2Flocalhost%3A3002%2Fredirect&state=84ae2b48-eb1b-4000-8782-ac1cd748aeb0&nonce=&request=`);
+    expect(fetchResult.headers.get("location")).toEqual(
+      `${authorizationEndpoint}?response_type=id_token%20code&display=&scope=openid%20profile%20offline_access&client_id=coolApp2&redirect_uri=http%3A%2F%2Flocalhost%3A3002%2Fredirect&state=84ae2b48-eb1b-4000-8782-ac1cd748aeb0&nonce=&request=`
+    );
   });
 
   test("Authorize with cookie after consent redirects you back to the app", async () => {
     // This test uses the consented coolApp2
-    const fetchResult = await fetch(`${authorizationEndpoint}?response_type=id_token%20code&display=&scope=openid%20profile%20offline_access&client_id=coolApp2&redirect_uri=http%3A%2F%2Flocalhost%3A3002%2Fredirect&state=84ae2b48-eb1b-4000-8782-ac1cd748aeb0&nonce=&request=`, {
-      "headers": {
-        cookie
-      },
-      redirect: "manual"
-    });
+    const fetchResult = await fetch(
+      `${authorizationEndpoint}?response_type=id_token%20code&display=&scope=openid%20profile%20offline_access&client_id=coolApp2&redirect_uri=http%3A%2F%2Flocalhost%3A3002%2Fredirect&state=84ae2b48-eb1b-4000-8782-ac1cd748aeb0&nonce=&request=`,
+      {
+        headers: {
+          cookie,
+        },
+        redirect: "manual",
+      }
+    );
     expect(fetchResult.status).toEqual(302);
-    expect(fetchResult.headers.get('location').startsWith("http://localhost:3002/redirect?code=")).toEqual(true);
+    expect(
+      fetchResult.headers
+        .get("location")
+        .startsWith("http://localhost:3002/redirect?code=")
+    ).toEqual(true);
   });
-
 });
--- test/surface/token.test.ts
+++ test/surface/token.test.ts
@@ -5,80 +5,82 @@ import fetch, { Response } from "node-fetch";
 import { subtle } from "isomorphic-webcrypto";
 import base64url from "base64url";
 import * as RSA from "node-rsa";
+import { getCookie } from "../helpers/getCookie";
 
-const debug = Debug('token tests');
+const debug = Debug("token tests");
 
 const ALICE_WEBID = process.env.ALICE_WEBID;
 const SERVER_ROOT = process.env.SERVER_ROOT || "https://server";
-const LOGIN_URL = `${SERVER_ROOT}/login/password`;
-const query2 = "?response_type=id_token%20code&redirect_uri=http%3A%2F%2Flocalhost%3A3002%2Fredirect&scope=openid%20profile%20offline_access&client_id=coolApp2&code_challenge_method=S256&code_challenge=M3CBok-0kQFc0GUz2YD90cFee0XzTTru3Eaj0Ubm-oc&state=84ae2b48-eb1b-4000-8782-ac1cd748aeb0";
+const LOGIN_URL = `${SERVER_ROOT}/login`;
+const query2 =
+  "?response_type=id_token%20code&redirect_uri=http%3A%2F%2Flocalhost%3A3002%2Fredirect&scope=openid%20profile%20offline_access&client_id=coolApp2&code_challenge_method=S256&code_challenge=M3CBok-0kQFc0GUz2YD90cFee0XzTTru3Eaj0Ubm-oc&state=84ae2b48-eb1b-4000-8782-ac1cd748aeb0";
 
-function hashClaim (value, hashLength) {
+function hashClaim(value, hashLength) {
   if (value) {
-    let alg = { name: `SHA-${hashLength}`}
-    let octets = new Buffer(value, 'ascii')
+    const alg = { name: `SHA-${hashLength}` };
+    const octets = new Buffer(value, "ascii");
 
-    return subtle.digest(alg, new Uint8Array(octets)).then(digest => {
-      let hash = Buffer.from(digest)
-      let half = hash.slice(0, hash.byteLength / 2)
-      return base64url(half)
-    })
+    return subtle.digest(alg, new Uint8Array(octets)).then((digest) => {
+      const hash = Buffer.from(digest);
+      const half = hash.slice(0, hash.byteLength / 2);
+      return base64url(half);
+    });
   }
 }
 
-async function getCookie() {
-  const result = await fetch(`${SERVER_ROOT}/login/password`, {
-    headers: {
-      "content-type": "application/x-www-form-urlencoded"
-    },
-    body: "username=alice&password=123",
-    method: "POST",
-    redirect: "manual"
-  });
-  return result.headers.get('set-cookie');
-}
-
 describe("The IODC token", () => {
-  let code
-  let idTokenJwt
-  let idTokenObj
-  let jwks
+  let code;
+  let idTokenJwt;
+  let idTokenObj;
+  let jwks;
   beforeAll(async () => {
     const configFetchResult = await fetch(
       `${SERVER_ROOT}/.well-known/openid-configuration`
     );
     const body = await configFetchResult.text();
     const configObj = JSON.parse(body);
-    const jwksResponse = await fetch(configObj.jwks_uri)
-    jwks = await jwksResponse.json()
-    debug('jwks', jwks)
-  
+    const jwksResponse = await fetch(configObj.jwks_uri);
+    jwks = await jwksResponse.json();
+    debug("jwks", jwks);
+
     const authorizationEndpoint = configObj.authorization_endpoint;
-    const cookie = await getCookie();
-    const authorizeFetchResult1 = await fetch(`${authorizationEndpoint}?response_type=id_token%20code&display=&scope=openid%20profile%20offline_access&client_id=coolApp2&redirect_uri=http%3A%2F%2Flocalhost%3A3002%2Fredirect&state=84ae2b48-eb1b-4000-8782-ac1cd748aeb0&nonce=&request=`, {
-      "headers": {
-        cookie
-      },
-      redirect: "manual"
-    });
+    const cookie = await getCookie(LOGIN_URL);
+    const authorizeFetchResult1 = await fetch(
+      `${authorizationEndpoint}?response_type=id_token%20code&display=&scope=openid%20profile%20offline_access&client_id=coolApp2&redirect_uri=http%3A%2F%2Flocalhost%3A3002%2Fredirect&state=84ae2b48-eb1b-4000-8782-ac1cd748aeb0&nonce=&request=`,
+      {
+        headers: {
+          cookie,
+        },
+        redirect: "manual",
+      }
+    );
     expect(authorizeFetchResult1.status).toEqual(302);
-    const authorizeFetchResult2 = await fetch(authorizeFetchResult1.headers.get('location'), {
-      "headers": {
-        cookie
-      },
-      redirect: "manual"
-    });
+    const authorizeFetchResult2 = await fetch(
+      authorizeFetchResult1.headers.get("location"),
+      {
+        headers: {
+          cookie,
+        },
+        redirect: "manual",
+      }
+    );
     expect(authorizeFetchResult2.status).toEqual(302);
-    const authorizeFetchResult3 = await fetch(authorizeFetchResult2.headers.get('location'), {
-      "headers": {
-        cookie
-      },
-      redirect: "manual"
-    });
+    const authorizeFetchResult3 = await fetch(
+      authorizeFetchResult2.headers.get("location"),
+      {
+        headers: {
+          cookie,
+        },
+        redirect: "manual",
+      }
+    );
     expect(authorizeFetchResult3.status).toEqual(302);
-    const callbackParams = authorizeFetchResult3.headers.get('location').substring('http://localhost:3002/redirect?'.length).split('&');
-    code = callbackParams[0].substring('code='.length);
-    idTokenJwt = callbackParams[1].substring('id_token='.length);
+    const callbackParams = authorizeFetchResult3.headers
+      .get("location")
+      .substring("http://localhost:3002/redirect?".length)
+      .split("&");
+    code = callbackParams[0].substring("code=".length);
+    idTokenJwt = callbackParams[1].substring("id_token=".length);
     idTokenObj = decode(idTokenJwt);
   });
 
@@ -106,12 +108,13 @@ describe("The IODC token", () => {
     expect(idTokenObj.sub).toEqual(ALICE_WEBID);
   });
   test("id token has an expiry time a few weeks in the future", async () => {
-    const futureWeeks = (idTokenObj.exp * 1000 - new Date().getTime()) / (7 * 24 * 3600 * 1000);
+    const futureWeeks =
+      (idTokenObj.exp * 1000 - new Date().getTime()) / (7 * 24 * 3600 * 1000);
     expect(futureWeeks).toBeGreaterThan(1);
     expect(futureWeeks).toBeLessThan(5);
   });
   test("id token has an issued-at time a few seconds in the past", async () => {
-    const pastMilliSeconds = (new Date().getTime() - idTokenObj.iat * 1000);
+    const pastMilliSeconds = new Date().getTime() - idTokenObj.iat * 1000;
     expect(pastMilliSeconds).toBeLessThan(60 * 1000);
     expect(pastMilliSeconds).toBeGreaterThan(0);
   });
@@ -125,21 +128,21 @@ describe("The IODC token", () => {
     // console.log(jwks.keys.map(key => key.kid));
     let found = false;
     jwks.keys.forEach((key) => {
-      if(found) {
+      if (found) {
         return;
       }
       const pubKeyComponents = {
-        e: Buffer.from(key.e, 'base64'),
-        n: Buffer.from(key.n, 'base64')
-      }
-      debug('pubKeyComponents', pubKeyComponents)
-      const rsaPubKey: RSA = new RSA()
-      rsaPubKey.importKey(pubKeyComponents, 'components-public')
-      const publicPem: string = rsaPubKey.exportKey('pkcs1-public-pem')
-      debug('publicPem', publicPem)
+        e: Buffer.from(key.e, "base64"),
+        n: Buffer.from(key.n, "base64"),
+      };
+      debug("pubKeyComponents", pubKeyComponents);
+      const rsaPubKey: RSA = new RSA();
+      rsaPubKey.importKey(pubKeyComponents, "components-public");
+      const publicPem: string = rsaPubKey.exportKey("pkcs1-public-pem");
+      debug("publicPem", publicPem);
       try {
         const result = verify(idTokenJwt, publicPem, {
-          algorithms: [ "RS256" ]
+          algorithms: ["RS256"],
         });
         // console.log(result, key.kid, 'yes');
         found = true;
