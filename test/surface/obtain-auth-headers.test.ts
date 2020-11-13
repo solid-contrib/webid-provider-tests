00b03ec23e084ae2ebbe5dd70f94c35521996aae
Michiel de Jong <michiel@unhosted.org>
2020-11-13 16:10:19 +0100

    check lengths

--- test/surface/obtain-auth-headers.test.ts
+++ test/surface/obtain-auth-headers.test.ts
@@ -4,10 +4,11 @@ test("can obtain auth headers", async () => {
   const cookie = process.env.COOKIE || "";
   const appOrigin = "https://tester";
   const authFetcher = await getAuthFetcher(oidcIssuer, cookie, appOrigin);
-  const authHeaders = getAuthHeaders(
+  const authHeaders = await getAuthHeaders(
     `${oidcIssuer}/example/url.txt`,
     "GET",
     authFetcher
   );
-  console.log(authHeaders);
+  expect(authHeaders.Authorization.length > 0).toBeTruthy();
+  expect(authHeaders.DPop.length > 0).toBeTruthy();
 });
